820364c03fea4c8e41ece4b9e0878ccb51c02350
SerepentH <79256227+SerepentH@users.noreply.github.com>
2022-04-15 13:47:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -12,7 +18,11 @@ function setup()
 
 function draw()
 {
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be = " + difference + "px";
     background('Black');
+    fill('salmon');
+    stroke('green');
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded()
@@ -25,5 +35,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
